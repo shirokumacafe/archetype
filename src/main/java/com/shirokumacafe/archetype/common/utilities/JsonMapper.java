d8d431540f39e1a1a7e1388212dcd40118b9aba5
SHIROKUMA <wengyuchen@163.com>
2013-07-19 12:26:52 +0800

    更新配置

--- src/main/java/com/shirokumacafe/archetype/common/utilities/JsonMapper.java
+++ src/main/java/com/shirokumacafe/archetype/common/utilities/JsonMapper.java
@@ -1,15 +1,22 @@
+
 package com.shirokumacafe.archetype.common.utilities;
 
-import com.fasterxml.jackson.annotation.JsonInclude;
-import com.fasterxml.jackson.core.JsonProcessingException;
-import com.fasterxml.jackson.databind.*;
-import com.fasterxml.jackson.databind.util.JSONPObject;
-import com.fasterxml.jackson.module.jaxb.JaxbAnnotationModule;
+import java.io.IOException;
+import java.util.Collection;
+import java.util.Map;
+
 import org.apache.commons.lang3.StringUtils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-import java.io.IOException;
+import com.fasterxml.jackson.annotation.JsonInclude.Include;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.DeserializationFeature;
+import com.fasterxml.jackson.databind.JavaType;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.SerializationFeature;
+import com.fasterxml.jackson.databind.util.JSONPObject;
+import com.fasterxml.jackson.module.jaxb.JaxbAnnotationModule;
 
 /**
  * 简单封装Jackson，实现JSON String<->Java Object的Mapper.
@@ -28,13 +35,13 @@ public class JsonMapper {
         this(null);
     }
 
-    public JsonMapper(JsonInclude.Include include) {
+    public JsonMapper(Include include) {
         mapper = new ObjectMapper();
-        //设置输出时包含属性的风格
+        // 设置输出时包含属性的风格
         if (include != null) {
             mapper.setSerializationInclusion(include);
         }
-        //设置输入时忽略在JSON字符串中存在但Java对象实际没有的属性
+        // 设置输入时忽略在JSON字符串中存在但Java对象实际没有的属性
         mapper.disable(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES);
     }
 
@@ -42,14 +49,14 @@ public class JsonMapper {
      * 创建只输出非Null且非Empty(如List.isEmpty)的属性到Json字符串的Mapper,建议在外部接口中使用.
      */
     public static JsonMapper nonEmptyMapper() {
-        return new JsonMapper(JsonInclude.Include.NON_EMPTY);
+        return new JsonMapper(Include.NON_EMPTY);
     }
 
     /**
      * 创建只输出初始值被改变的属性到Json字符串的Mapper, 最节约的存储方式，建议在内部接口中使用。
      */
     public static JsonMapper nonDefaultMapper() {
-        return new JsonMapper(JsonInclude.Include.NON_DEFAULT);
+        return new JsonMapper(Include.NON_DEFAULT);
     }
 
     /**
@@ -66,22 +73,6 @@ public class JsonMapper {
             return null;
         }
     }
-    /**
-     * Class是POJO中使用@JsonView设定的,过滤用
-     * Object可以是POJO，也可以是Collection或数组。
-     * 如果对象为Null, 返回"null".
-     * 如果集合为空集合, 返回"[]".
-     */
-    public String toJson(Class clazz,Object object) {
-
-        try {
-            ObjectWriter objectWriter = mapper.writerWithView(clazz);
-            return objectWriter.writeValueAsString(object);
-        } catch (IOException e) {
-            logger.warn("write to json string error:" + object, e);
-            return null;
-        }
-    }
 
     /**
      * 反序列化POJO或简单Collection如List<String>.
@@ -89,8 +80,9 @@ public class JsonMapper {
      * 如果JSON字符串为Null或"null"字符串, 返回Null.
      * 如果JSON字符串为"[]", 返回空集合.
      *
-     * 如需反序列化复杂Collection如List<MyBean>, 请使用fromJson(String,JavaType)
-     * @see #fromJson(String, com.fasterxml.jackson.databind.JavaType)
+     * 如需反序列化复杂Collection如List<MyBean>, 请使用fromJson(String, JavaType)
+     *
+     * @see #fromJson(String, JavaType)
      */
     public <T> T fromJson(String jsonString, Class<T> clazz) {
         if (StringUtils.isEmpty(jsonString)) {
@@ -106,10 +98,10 @@ public class JsonMapper {
     }
 
     /**
-     * 反序列化复杂Collection如List<Bean>, 先使用函數createCollectionType构造类型,然后调用本函数.
+     * 反序列化复杂Collection如List<Bean>, 先使用createCollectionType()或contructMapType()构造类型, 然后调用本函数.
+     *
      * @see #createCollectionType(Class, Class...)
      */
-    @SuppressWarnings(value = "unchecked")
     public <T> T fromJson(String jsonString, JavaType javaType) {
         if (StringUtils.isEmpty(jsonString)) {
             return null;
@@ -124,21 +116,25 @@ public class JsonMapper {
     }
 
     /**
-     * 構造泛型的Collection Type如:
-     * ArrayList<MyBean>, 则调用constructCollectionType(ArrayList.class,MyBean.class)
-     * HashMap<String,MyBean>, 则调用(HashMap.class,String.class, MyBean.class)
+     * 构造Collection类型.
      */
-    public JavaType createCollectionType(Class<?> collectionClass, Class<?>... elementClasses) {
-        return mapper.getTypeFactory().constructParametricType(collectionClass, elementClasses);
+    public JavaType contructCollectionType(Class<? extends Collection> collectionClass, Class<?> elementClass) {
+        return mapper.getTypeFactory().constructCollectionType(collectionClass, elementClass);
+    }
+
+    /**
+     * 构造Map类型.
+     */
+    public JavaType contructMapType(Class<? extends Map> mapClass, Class<?> keyClass, Class<?> valueClass) {
+        return mapper.getTypeFactory().constructMapType(mapClass, keyClass, valueClass);
     }
 
     /**
      * 當JSON裡只含有Bean的部分屬性時，更新一個已存在Bean，只覆蓋該部分的屬性.
      */
-    @SuppressWarnings(value = "unchecked")
     public <T> T update(String jsonString, T object) {
         try {
-            return (T) mapper.readerForUpdating(object).readValue(jsonString);
+            return mapper.readerForUpdating(object).readValue(jsonString);
         } catch (JsonProcessingException e) {
             logger.warn("update json string:" + jsonString + " to object:" + object + " error.", e);
         } catch (IOException e) {
