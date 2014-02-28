b2a42d89c8cb60e40a3239ff46e5e3566da900b9
shirokuma <wengyuchen@aliyun.com>
2014-02-28 11:35:42 +0800

    更新帮助类

--- src/main/java/com/shirokumacafe/archetype/common/utilities/Servlets.java
+++ src/main/java/com/shirokumacafe/archetype/common/utilities/Servlets.java
@@ -1,20 +1,19 @@
-/**
- * Copyright (c) 2005-2012 springside.org.cn
+/*******************************************************************************
+ * Copyright (c) 2005, 2014 springside.github.io
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
- */
+ *******************************************************************************/
 package com.shirokumacafe.archetype.common.utilities;
 
+import com.google.common.base.Charsets;
+import com.google.common.net.HttpHeaders;
 import org.apache.commons.lang3.Validate;
 
 import javax.servlet.ServletRequest;
+import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-import java.io.UnsupportedEncodingException;
-import java.util.Enumeration;
-import java.util.Iterator;
-import java.util.Map;
+import java.util.*;
 import java.util.Map.Entry;
-import java.util.TreeMap;
 
 /**
  * Http与Servlet工具类.
@@ -23,18 +22,104 @@ import java.util.TreeMap;
  */
 public class Servlets {
 
+	// -- 常用数值定义 --//
+	public static final long ONE_YEAR_SECONDS = 60 * 60 * 24 * 365;
+
+	/**
+	 * 设置客户端缓存过期时间 的Header.
+	 */
+	public static void setExpiresHeader(HttpServletResponse response, long expiresSeconds) {
+		// Http 1.0 header, set a fix expires date.
+		response.setDateHeader(HttpHeaders.EXPIRES, System.currentTimeMillis() + (expiresSeconds * 1000));
+		// Http 1.1 header, set a time after now.
+		response.setHeader(HttpHeaders.CACHE_CONTROL, "private, max-age=" + expiresSeconds);
+	}
+
+	/**
+	 * 设置禁止客户端缓存的Header.
+	 */
+	public static void setNoCacheHeader(HttpServletResponse response) {
+		// Http 1.0 header
+		response.setDateHeader(HttpHeaders.EXPIRES, 1L);
+		response.addHeader(HttpHeaders.PRAGMA, "no-cache");
+		// Http 1.1 header
+		response.setHeader(HttpHeaders.CACHE_CONTROL, "no-cache, no-store, max-age=0");
+	}
+
+	/**
+	 * 设置LastModified Header.
+	 */
+	public static void setLastModifiedHeader(HttpServletResponse response, long lastModifiedDate) {
+		response.setDateHeader(HttpHeaders.LAST_MODIFIED, lastModifiedDate);
+	}
+
+	/**
+	 * 设置Etag Header.
+	 */
+	public static void setEtag(HttpServletResponse response, String etag) {
+		response.setHeader(HttpHeaders.ETAG, etag);
+	}
+
+	/**
+	 * 根据浏览器If-Modified-Since Header, 计算文件是否已被修改.
+	 * 
+	 * 如果无修改, checkIfModify返回false ,设置304 not modify status.
+	 * 
+	 * @param lastModified 内容的最后修改时间.
+	 */
+	public static boolean checkIfModifiedSince(HttpServletRequest request, HttpServletResponse response,
+			long lastModified) {
+		long ifModifiedSince = request.getDateHeader(HttpHeaders.IF_MODIFIED_SINCE);
+		if ((ifModifiedSince != -1) && (lastModified < (ifModifiedSince + 1000))) {
+			response.setStatus(HttpServletResponse.SC_NOT_MODIFIED);
+			return false;
+		}
+		return true;
+	}
+
+	/**
+	 * 根据浏览器 If-None-Match Header, 计算Etag是否已无效.
+	 * 
+	 * 如果Etag有效, checkIfNoneMatch返回false, 设置304 not modify status.
+	 * 
+	 * @param etag 内容的ETag.
+	 */
+	public static boolean checkIfNoneMatchEtag(HttpServletRequest request, HttpServletResponse response, String etag) {
+		String headerValue = request.getHeader(HttpHeaders.IF_NONE_MATCH);
+		if (headerValue != null) {
+			boolean conditionSatisfied = false;
+			if (!"*".equals(headerValue)) {
+				StringTokenizer commaTokenizer = new StringTokenizer(headerValue, ",");
+
+				while (!conditionSatisfied && commaTokenizer.hasMoreTokens()) {
+					String currentToken = commaTokenizer.nextToken();
+					if (currentToken.trim().equals(etag)) {
+						conditionSatisfied = true;
+					}
+				}
+			} else {
+				conditionSatisfied = true;
+			}
+
+			if (conditionSatisfied) {
+				response.setStatus(HttpServletResponse.SC_NOT_MODIFIED);
+				response.setHeader(HttpHeaders.ETAG, etag);
+				return false;
+			}
+		}
+		return true;
+	}
+
 	/**
 	 * 设置让浏览器弹出下载对话框的Header.
 	 * 
 	 * @param fileName 下载后的文件名.
 	 */
 	public static void setFileDownloadHeader(HttpServletResponse response, String fileName) {
-		try {
-			// 中文文件名支持
-			String encodedfileName = new String(fileName.getBytes(), "ISO8859-1");
-			response.setHeader("Content-Disposition", "attachment; filename=\"" + encodedfileName + "\"");
-		} catch (UnsupportedEncodingException e) {
-		}
+		// 中文文件名支持
+		String encodedfileName = new String(fileName.getBytes(), Charsets.ISO_8859_1);
+		response.setHeader(HttpHeaders.CONTENT_DISPOSITION, "attachment; filename=\"" + encodedfileName + "\"");
+
 	}
 
 	/**
@@ -49,12 +134,12 @@ public class Servlets {
 		if (prefix == null) {
 			prefix = "";
 		}
-		while (paramNames != null && paramNames.hasMoreElements()) {
+		while ((paramNames != null) && paramNames.hasMoreElements()) {
 			String paramName = (String) paramNames.nextElement();
 			if ("".equals(prefix) || paramName.startsWith(prefix)) {
 				String unprefixed = paramName.substring(prefix.length());
 				String[] values = request.getParameterValues(paramName);
-				if (values == null || values.length == 0) {
+				if ((values == null) || (values.length == 0)) {
 					// Do nothing, no values found at all.
 				} else if (values.length > 1) {
 					params.put(unprefixed, values);
@@ -72,7 +157,7 @@ public class Servlets {
 	 * @see #getParametersStartingWith
 	 */
 	public static String encodeParameterStringWithPrefix(Map<String, Object> params, String prefix) {
-		if (params == null || params.size() == 0) {
+		if (Collections3.isEmpty(params)) {
 			return "";
 		}
 
